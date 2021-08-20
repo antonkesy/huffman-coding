dc71c5ba49f88ac73b862f336ddb89994c569240
Anton Kesy <anton@kesy.de>
2021-08-20 12:04:00 +0200

    Update Test.c

--- Test.c
+++ Test.c
@@ -9,28 +9,22 @@
 
 int testHuffman()
 {
-    char *testString = "123456";
+    char *testString = "Only need to fix the last byte to be written complete and cut. There are also many memory leaks :)";
     HuffmanData *hd = GetHuffmanData(testString, strlen(testString));
     size_t destSize = getItemsSum(hd->items);
     char *dest = malloc(destSize + 1);
     decodeHuffmanData(hd, dest, destSize);
     dest[destSize] = '\0';
     printf("%s\n%s\n", testString, dest);
-    printf("compare should be 0 = %u", strcmp(testString, dest));
-    return 0;
+    size_t compareValue = strcmp(testString, dest);
+    printf("compare should be 0 = %u", compareValue);
+    return compareValue;
 }
 
 int main(void)
 {
-    if (testPriorityQueue() != 0)
-    {
-        printf("priority queue test failed");
-    }
-
-    if (testHuffman() != 0)
-    {
-        printf("huffman test failed");
-    }
+    // printf("priority queue test failed ? %i", testPriorityQueue());
+    printf("huffman test failed ? %i", testHuffman());
 
 #ifndef DEBUG
     //getchar();
