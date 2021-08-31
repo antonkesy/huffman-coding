60d78bbdd2e9336c01c9c7dc3dff7dc21be41fda
Anton Kesy <anton@kesy.de>
2021-08-31 16:04:15 +0200

    add sanity checks & C11 _s

--- example_usage.c
+++ example_usage.c
@@ -8,87 +8,116 @@
 #define TEST_CODED_FILE_NAME "coded.txt"
 #define TEST_OUTPUT_FILE_NAME "output.txt"
 
-void create_test_input_file()
+void create_test_input_file(void)
 {
-    FILE *fp;
-    fp = fopen(TEST_INPUT_FILE_NAME, "w+");
-    if (fp != NULL)
-    {
-        fprintf(fp, "Hello,\tI'm a testing file :)\n");
-        for (register int i = 0; i < 100; ++i)
-        {
-            fprintf(fp, "number iteration = %i\n", i);
-        }
-    }
-    fclose(fp);
+   FILE** fp = malloc(sizeof(FILE**));
+   if (fp != NULL)
+   {
+      fopen_s(fp, TEST_INPUT_FILE_NAME, "w+");
+      if (*fp != NULL)
+      {
+         fprintf(*fp, "Hello,\tI'm a testing file :)\n");
+         for (register int i = 0; i < 100; ++i)
+         {
+            fprintf(*fp, "number iteration = %i\n", i);
+         }
+         fclose(*fp);
+      }
+   }
 }
 
-void example_usage_files()
+void example_usage_files(void)
 {
-    create_test_input_file();
-    FILE *src = fopen(TEST_INPUT_FILE_NAME, "r");
-    if (src == NULL)
-    {
-        printf("Unable to open file.\n");
-        return;
-    }
+   create_test_input_file();
+   FILE** src = malloc(sizeof(FILE**));
+   if (src != NULL)
+   {
+      fopen_s(src, TEST_INPUT_FILE_NAME, "r");
+      if (*src == NULL)
+      {
+         printf("Unable to open file.\n");
+         return;
+      }
 
-    FILE *coded_file = fopen(TEST_CODED_FILE_NAME, "w");
-    if (coded_file == NULL)
-    {
-        printf("Unable to create file.\n");
-        return;
-    }
+      FILE** coded_file = malloc(sizeof(FILE**));
+      if (coded_file != NULL)
+      {
+         fopen_s(coded_file, TEST_CODED_FILE_NAME, "w");
+         if (*coded_file == NULL)
+         {
+            printf("Unable to create file.\n");
+            return;
+         }
 
-    huffman_code_file_to_file(src, coded_file);
+         huffman_code_file_to_file(*src, *coded_file);
 
-    fclose(src);
-    fclose(coded_file);
+         fclose(*src);
+         fclose(*coded_file);
 
-    coded_file = fopen(TEST_CODED_FILE_NAME, "r");
-    if (coded_file == NULL)
-    {
-        printf("Unable to open file.\n");
-        return;
-    }
+         fopen_s(coded_file, TEST_CODED_FILE_NAME, "r");
+         if (*coded_file == NULL)
+         {
+            printf("Unable to open file.\n");
+            return;
+         }
 
-    FILE *dest = fopen(TEST_OUTPUT_FILE_NAME, "w");
-    if (dest == NULL)
-    {
-        printf("Unable to create file.\n");
-        return;
-    }
+         FILE** dest = malloc(sizeof(FILE**));
+         if (dest != NULL)
+         {
+            fopen_s(dest, TEST_OUTPUT_FILE_NAME, "w");
+            if (*dest == NULL)
+            {
+               printf("Unable to create file.\n");
+               return;
+            }
 
-    huffman_decode_file_to_file(coded_file, dest);
+            huffman_decode_file_to_file(*coded_file, *dest);
 
-    printf("files equal ? %i\n", files_equal(coded_file, dest));
+            printf("files equal ? %i\n", files_equal(*coded_file, *dest));
 
-    fclose(coded_file);
-    fclose(dest);
+            fclose(*coded_file);
+            fclose(*dest);
+         }
+      }
+   }
 }
 
-void example_usage()
+void example_usage(void)
 {
-    //get unsigned char array of data to convert
-    char *example_string = "BCAADDDCCACACAC";
-    //create huffman data
-    HuffmanData *hd = code_into_huffmanData((unsigned char *)example_string, strlen(example_string));
-    //need to transfer whole data to decode into original
+   //get unsigned char array of data to convert
+   char* example_string = "BCAADDDCCACACAC";
+   //create huffman data
+   HuffmanData* hd = code_into_huffmanData((unsigned char*)example_string, strlen(example_string));
+   //need to transfer whole data to decode into original
 
-    //create pointer to unsigned char arrays
-    unsigned char **decoded = (unsigned char **)malloc(sizeof(unsigned char **));
-    //if you need size of the original unsigend char arrays
-    size_t output_size;
-    //decodes huffman data into decoded
-    decode_huffman_data(hd, decoded, &output_size);
-    printf("%s\n", *decoded);
-    //done?
-    delete_huffman_data(hd);
+   //create pointer to unsigned char arrays
+   unsigned char** decoded = (unsigned char**)malloc(sizeof(unsigned char**));
+   if (decoded != NULL)
+   {
+      //if you need size of the original unsigend char arrays
+      size_t output_size;
+      //decodes huffman data into decoded
+      decode_huffman_data(hd, decoded, &output_size);
+      if (*decoded != NULL)
+      {
+         for (size_t i = 0U; i < output_size; ++i)
+         {
+            printf_s("%c", (*decoded)[i]);
+         }
+         printf("\n");
+      }
+      else
+      {
+         printf("decoded is null!\n");
+      }
+      //done?
+      delete_huffman_data(hd);
+   }
 }
 
 int main(void)
 {
-    example_usage();
-    example_usage_files();
-    return 0;
+   example_usage();
+   example_usage_files();
+   return 0;
 }
