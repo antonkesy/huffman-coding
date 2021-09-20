dd8820218d65c0f4797153afb772bbd83c964146
Anton Kesy <anton@kesy.de>
2021-09-20 08:21:08 +0200

    use stdint

--- utility/independent_int.c
+++ utility/independent_int.c
@@ -7,23 +7,23 @@ void *fill_iuint_x(const void *in, const size_t size_of_iuint) {
     if (return_value != NULL) {
         for (size_t i = 0U; i < size_of_iuint; ++i) {
 #ifdef BIG_ENDIAN
-            ((unsigned char *) return_value)[i] = ((unsigned char *) in)[size_of_iuint - i - 1];
+            ((uint8_t *) return_value)[i] = ((uint8_t *) in)[size_of_iuint - i - 1];
 #else
-            ((unsigned char *) return_value)[i] = ((unsigned char *) in)[i];
+            ((uint8_t *) return_value)[i] = ((uint8_t *) in)[i];
 #endif
         }
     }
     return return_value;
 }
 
-void *get_iunit_x_value(const void *iunit_in, const size_t size_of_iuint) {
+void *get_iuint_x_value(const void *iuint_in, const size_t size_of_iuint) {
     void *return_value = calloc(size_of_iuint, 1);
     if (return_value != NULL) {
         for (size_t i = 0U; i < size_of_iuint; ++i) {
 #ifdef BIG_ENDIAN
-            ((unsigned char *) return_value)[i] = ((unsigned char *) iunit_in)[size_iuint - i - 1];
+            ((uint8_t *) return_value)[i] = ((uint8_t *) iunit_in)[size_iuint - i - 1];
 #else
-            ((unsigned char *) return_value)[i] = ((unsigned char *) iunit_in)[i];
+            ((uint8_t *) return_value)[i] = ((uint8_t *) iuint_in)[i];
 #endif
         }
     }
@@ -35,7 +35,7 @@ iuint_16_t *fill_iuint_16(const uint16_t *in) {
 }
 
 uint16_t *get_iuint_16_value(const iuint_16_t *in) {
-    return get_iunit_x_value(in, sizeof(iuint_16_t));
+    return get_iuint_x_value(in, sizeof(iuint_16_t));
 }
 
 iuint_32_t *fill_iuint_32(const uint32_t *in) {
@@ -43,7 +43,7 @@ iuint_32_t *fill_iuint_32(const uint32_t *in) {
 }
 
 uint32_t *get_iuint_32_value(const iuint_32_t *in) {
-    return get_iunit_x_value(in, sizeof(iuint_32_t));
+    return get_iuint_x_value(in, sizeof(iuint_32_t));
 }
 
 iuint_64_t *fill_iuint_64(const uint64_t *in) {
@@ -51,5 +51,5 @@ iuint_64_t *fill_iuint_64(const uint64_t *in) {
 }
 
 uint64_t *get_iuint_64_value(const iuint_64_t *in) {
-    return get_iunit_x_value(in, sizeof(iuint_64_t));
+    return get_iuint_x_value(in, sizeof(iuint_64_t));
 }
--- utility/independent_int.h
+++ utility/independent_int.h
@@ -11,29 +11,27 @@
 #endif
 
 typedef struct {
-    unsigned char bytes[2];
+    uint8_t bytes[2];
 } iuint_16_t;
 
 typedef struct {
-    unsigned char bytes[4];
+    uint8_t bytes[4];
 } iuint_32_t;
 
 typedef struct {
-    unsigned char bytes[8];
+    uint8_t bytes[8];
 } iuint_64_t;
 
-void *fill_iuint_x(const void *in, const size_t size_of_iuint);
+void *fill_iuint_x(const void *in,const size_t size_of_iuint);
+void *get_iuint_x_value(const void *iuint_in, const size_t size_of_iuint);
 
 iuint_16_t *fill_iuint_16(const uint16_t *in);
-
 uint16_t *get_iuint_16_value(const iuint_16_t *in);
 
 iuint_32_t *fill_iuint_32(const uint32_t *in);
-
 uint32_t *get_iuint_32_value(const iuint_32_t *in);
 
 iuint_64_t *fill_iuint_64(const uint64_t *in);
-
 uint64_t *get_iuint_64_value(const iuint_64_t *in);
 
 #endif //HUFFMAN_TEST_INDEPENDENT_INT_H
