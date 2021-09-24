74bcd0b3bfd250702c08872817e6dbb668f48d36
Anton Kesy <anton@kesy.de>
2021-09-24 12:25:32 +0200

    Delete huffman_utility_test.h

--- test/tests/huffman_utility_test.h
+++
@@ -1,9 +0,0 @@
-#ifndef _HUFFMAN_UTILITY_TEST_H
-#define _HUFFMAN_UTILITY_TEST_H
-
-int test_equal_sort_item();
-int test_equal_sorted_items();
-int test_is_huffman_data_equal();
-int test_coded_string_equal();
-
-#endif
