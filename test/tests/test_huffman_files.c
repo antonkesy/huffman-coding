56b7468b1d254952ab92615cb075847e8b86f2d1
Anton Kesy <anton@kesy.de>
2021-09-25 19:45:17 +0200

    read files in binary
    yeah ... took me a while :/

--- test/tests/test_huffman_files.c
+++ test/tests/test_huffman_files.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <malloc.h>
 #include "../test_utility.h"
 #include "../../utility/huffman_file_to_file.h"
 
@@ -13,15 +12,14 @@
 bool test_file_coding(void)
 {
     create_test_input_file(TEST_INPUT_FILE_NAME, 0xFFF);
-    FILE *src = fopen(TEST_INPUT_FILE_NAME, "r");
-
+    FILE *src = fopen("300_2.mp4", "rb");
     if (src == NULL || ferror(src))
     {
         printf("Unable to open file.\n");
         return 1;
     }
 
-    FILE *coded_file = fopen(TEST_CODED_FILE_NAME, "w");
+    FILE *coded_file = fopen(TEST_CODED_FILE_NAME, "wb");
     if (coded_file == NULL || ferror(coded_file))
     {
         printf("Unable to create file.\n");
@@ -40,7 +38,7 @@ bool test_file_coding(void)
         return 1;
     }
 
-    FILE *dest = fopen(TEST_OUTPUT_FILE_NAME, "w");
+    FILE *dest = fopen(TEST_OUTPUT_FILE_NAME, "wb+");
     if (dest == NULL || ferror(dest))
     {
         printf("Unable to create file.\n");
@@ -49,9 +47,18 @@ bool test_file_coding(void)
 
     huffman_decode_file_to_file(coded_file, dest);
 
-    const bool are_files_equal = files_equal(coded_file, dest);
-
     fclose(coded_file);
+
+    src = fopen(TEST_INPUT_FILE_NAME, "rb");
+    if (src == NULL || ferror(src))
+    {
+        printf("Unable to open file.\n");
+        return 1;
+    }
+
+
+    const bool are_files_equal = files_equal(src, dest);
+
     fclose(dest);
 
     return are_files_equal;
