44c40d67ffff42ab12448547a0816c2edcd0acd6
Anton Kesy <anton@kesy.de>
2021-09-25 20:44:21 +0200

    add test for big file

--- test/tests/test_huffman_files.c
+++ test/tests/test_huffman_files.c
@@ -2,22 +2,33 @@
 #include "../test_utility.h"
 #include "../../utility/huffman_file_to_file.h"
 
-#define TEST_INPUT_FILE_NAME "input.txt"
-#define TEST_CODED_FILE_NAME "coded.txt"
-#define TEST_OUTPUT_FILE_NAME "output.txt"
-
 bool test_file_coding(void)
 {
-    create_test_input_file(TEST_INPUT_FILE_NAME, 0xFFF);
+    const char *input_file = "input_file.txt";
+    const char *coded_file = "coded_file.txt";
+    const char *output_file = "output_file.txt";
 
-    huffman_code_file_to_file(TEST_INPUT_FILE_NAME, TEST_CODED_FILE_NAME);
+    create_test_input_file(input_file, 0xFFF);
+    huffman_code_file_to_file(input_file, coded_file);
+    huffman_decode_file_to_file(coded_file, output_file);
 
-    huffman_decode_file_to_file(TEST_CODED_FILE_NAME, TEST_OUTPUT_FILE_NAME);
+    return files_equal(input_file, output_file);
+}
 
-    return files_equal(TEST_INPUT_FILE_NAME, TEST_OUTPUT_FILE_NAME);
+bool test_big_file_coding(void)
+{
+    const char *input_file = "big_input_file.txt";
+    const char *coded_file = "big_coded_file.txt";
+    const char *output_file = "big_output_file.txt";
+
+    create_test_big_input_file(input_file);
+    huffman_code_file_to_file(input_file, coded_file);
+    huffman_decode_file_to_file(coded_file, output_file);
+
+    return files_equal(input_file, output_file);
 }
 
 int main(void)
 {
-    return !test_file_coding();
+    return !(test_file_coding() && test_big_file_coding());
 }
