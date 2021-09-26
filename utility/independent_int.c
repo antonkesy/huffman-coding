e3226e95ad71db8b86aae59459d141cf473c05bc
Anton Kesy <anton@kesy.de>
2021-09-26 15:21:11 +0200

    update wrong macro

--- utility/independent_int.c
+++ utility/independent_int.c
@@ -9,11 +9,11 @@ void *fill_iuint_x(const void *in, const size_t size_of_iuint)
     {
         for (size_t i = 0U; i < size_of_iuint; ++i)
         {
-            ((uint8_t *)return_value)[i] = ((uint8_t *)in)[
+            ((uint8_t *) return_value)[i] = ((uint8_t *) in)[
 #ifdef BIG_ENDIAN_II_H
-                size_of_iuint - i - 1
+                    size_of_iuint - i - 1
 #else
-                i
+                    i
 #endif
             ];
         }
@@ -28,11 +28,11 @@ void *get_iuint_x_value(const void *iuint_in, const size_t size_of_iuint)
     {
         for (size_t i = 0U; i < size_of_iuint; ++i)
         {
-            ((uint8_t *)return_value)[i] = ((uint8_t *)iuint_in)[
-#ifdef BIG_ENDIAN
-                size_of_iuint - i - 1
+            ((uint8_t *) return_value)[i] = ((uint8_t *) iuint_in)[
+#ifdef BIG_ENDIAN_II_H
+                    size_of_iuint - i - 1
 #else
-                i
+                    i
 #endif
             ];
         }
@@ -42,30 +42,30 @@ void *get_iuint_x_value(const void *iuint_in, const size_t size_of_iuint)
 
 iuint_16_t *fill_iuint_16(const uint16_t *in)
 {
-    return (iuint_16_t *)fill_iuint_x(in, sizeof(iuint_16_t));
+    return (iuint_16_t *) fill_iuint_x(in, sizeof(iuint_16_t));
 }
 
 uint16_t *get_iuint_16_value(const iuint_16_t *in)
 {
-    return (uint16_t *)get_iuint_x_value(in, sizeof(iuint_16_t));
+    return (uint16_t *) get_iuint_x_value(in, sizeof(iuint_16_t));
 }
 
 iuint_32_t *fill_iuint_32(const uint32_t *in)
 {
-    return (iuint_32_t *)fill_iuint_x(in, sizeof(iuint_32_t));
+    return (iuint_32_t *) fill_iuint_x(in, sizeof(iuint_32_t));
 }
 
 uint32_t *get_iuint_32_value(const iuint_32_t *in)
 {
-    return (uint32_t *)get_iuint_x_value(in, sizeof(iuint_32_t));
+    return (uint32_t *) get_iuint_x_value(in, sizeof(iuint_32_t));
 }
 
 iuint_64_t *fill_iuint_64(const uint64_t *in)
 {
-    return (iuint_64_t *)fill_iuint_x(in, sizeof(iuint_64_t));
+    return (iuint_64_t *) fill_iuint_x(in, sizeof(iuint_64_t));
 }
 
 uint64_t *get_iuint_64_value(const iuint_64_t *in)
 {
-    return (uint64_t *)get_iuint_x_value(in, sizeof(iuint_64_t));
+    return (uint64_t *) get_iuint_x_value(in, sizeof(iuint_64_t));
 }
