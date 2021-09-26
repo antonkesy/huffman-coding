3143cde21df85062573afacb2ed30718e84f4e48
Anton Kesy <anton@kesy.de>
2021-09-26 07:45:07 +0200

    Update test_huffman_simple.c

--- test/tests/test_huffman_simple.c
+++ test/tests/test_huffman_simple.c
@@ -1,21 +1,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../../huffman.h"
+#include "../../utility/huffman_utility.h"
 
 int main()
 {
-   char* test_string = "Were you able to open it before, on the computer you're using now? XYC23423";
-   HuffmanData* hd = code_into_huffman_data((unsigned char*)test_string, strlen(test_string));
-   unsigned char** dest = (unsigned char**)malloc(sizeof(unsigned char**));
-   if (dest == NULL)
-   {
-      return 1;
-   }
-   size_t output_size = 0;
-   decode_huffman_data(hd, dest, &output_size);
-   if (*dest == NULL)
-   {
-      return 1;
-   }
-   return strcmp(test_string, (char*)*dest);
+    char *test_string = "Were you able to open it before, on the computer you're using now? XYC23423";
+    HuffmanData *hd = code_into_huffman_data((uint8_t *) test_string, strlen(test_string));
+    unsigned char **dest = (uint8_t **) malloc(sizeof(uint8_t **));
+    if (dest == NULL)
+    {
+        return 1;
+    }
+    size_t output_size = 0;
+    decode_huffman_data(hd, dest, &output_size);
+    if (*dest == NULL)
+    {
+        return 1;
+    }
+    return !is_coded_string_equal((uint8_t *) test_string, (uint8_t *) *dest, strlen(test_string));
 }
