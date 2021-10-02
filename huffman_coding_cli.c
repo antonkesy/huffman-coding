19f742ea6936fb3081d94f81adc89691839f1544
Anton Kesy <anton@kesy.de>
2021-10-02 11:49:01 +0200

    create base for command line interface

---
+++ huffman_coding_cli.c
@@ -0,0 +1,72 @@
+#include <string.h>
+#include "huffman_coding_cli.h"
+#include "utility/huffman_file_to_file.h"
+
+
+int process_input_arguments(int argc, char **argv)
+{
+    if (argc <= 1)
+    {
+        return 1;
+    }
+    long huffman_buffer_size = BUFF_SIZE_FILE;
+    char *input_file_name = NULL;
+    char *output_file_name = NULL;
+    for (int i = 0; i < argc; ++i)
+    {
+        switch (decode_argument(argv[i]))
+        {
+            case Error:
+                print_argument_error();
+                return 1;
+            case Version:
+                print_version();
+                break;
+            case Info:
+                print_info();
+                break;
+            case BufferSize:
+                huffman_buffer_size = get_argument_long_value(argv[i], strlen(BufferSizeArgument));
+                break;
+            case InputFileName:
+                input_file_name = get_argument_string(argv[i], strlen(InputFileNameArgument));
+                break;
+            case OutputFileName:
+                output_file_name = get_argument_string(argv[i], strlen(OutputFileNameArgument));
+                break;
+        }
+    }
+    huffman_code_file_to_file(input_file_name, output_file_name);
+    return 0;
+}
+
+enum ArgumentOptions decode_argument(char *arg)
+{
+    //perror and break if unknown
+    return Error;
+}
+
+void print_argument_error()
+{
+
+}
+
+void print_version()
+{
+
+}
+
+void print_info()
+{
+
+}
+
+long get_argument_long_value(char *arg, int argument_title_length)
+{
+    return 0;
+}
+
+char *get_argument_string(char *arg, int argument_title_length)
+{
+    return 0;
+}
---
+++ huffman_coding_cli.h
@@ -0,0 +1,27 @@
+#ifndef HUFFMAN_HUFFMAN_CODING_CLI_H
+#define HUFFMAN_HUFFMAN_CODING_CLI_H
+
+#define BufferSizeArgument "-buffSize="
+#define InputFileNameArgument ""
+#define OutputFileNameArgument ""
+
+enum ArgumentOptions
+{
+    Error = 0, Version, Info, BufferSize, InputFileName, OutputFileName
+};
+
+int process_input_arguments(int argc, char **argv);
+
+enum ArgumentOptions decode_argument(char *arg);
+
+void print_argument_error();
+
+void print_version();
+
+void print_info();
+
+long get_argument_long_value(char *arg, int argument_title_length);
+
+char *get_argument_string(char *arg, int argument_title_length);
+
+#endif //HUFFMAN_HUFFMAN_CODING_CLI_H
