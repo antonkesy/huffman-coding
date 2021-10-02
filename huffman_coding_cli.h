1d1387a0c88c77d420418dfdfeb5dd7248f02d27
Anton Kesy <anton@kesy.de>
2021-10-02 18:44:01 +0200

    formatting

--- huffman_coding_cli.h
+++ huffman_coding_cli.h
@@ -19,9 +19,9 @@ enum ArgumentOptions
     Error = 0, Version, Info, BufferSize, OutputFileName, CodeMode, DecodeMode
 };
 
-int process_input_arguments(int argc, char **argv);
+int process_input_arguments(int argc, char** argv);
 
-enum ArgumentOptions decode_argument(const char *arg);
+enum ArgumentOptions decode_argument(const char* arg);
 
 void print_argument_error();
 
@@ -29,14 +29,14 @@ void print_version();
 
 void print_info();
 
-long get_argument_long_value(char *arg, int argument_title_length);
+long get_argument_long_value(char* arg, int argument_title_length);
 
-long get_buffer_size_from_argument(char *argument);
+long get_buffer_size_from_argument(char* argument);
 
-bool is_argument_x(const char *arg, const char *argument_def);
+bool is_argument_x(const char* arg, const char* argument_def);
 
-const char *remove_leading_whitespaces(char *in);
+const char* remove_leading_whitespaces(char* in);
 
-int set_default_output_file_name(char **out_output_file_name, const char *input_file_name);
+int set_default_output_file_name(char** out_output_file_name, const char* input_file_name);
 
 #endif //HUFFMAN_HUFFMAN_CODING_CLI_H
