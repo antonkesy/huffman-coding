9fcaf6225a99f51d713025be243f53c78dc89d67
Anton Kesy <anton@kesy.de>
2021-10-02 14:05:53 +0200

    Update test_cli.c

--- test/tests/test_cli.c
+++ test/tests/test_cli.c
@@ -7,7 +7,7 @@ int test_error()
     int process_return_value = 0;
     process_return_value += process_input_arguments(0, NULL);
     char *bad_arguments = "nothing :)";
-    process_return_value += process_input_arguments(2, &bad_arguments);
+    process_return_value += process_input_arguments(1, &bad_arguments);
 
     return process_return_value == 2 ? 0 : 1;
 }
@@ -19,6 +19,9 @@ int test_buffer_size()
     int process_return_value = 0;
     char *buffer_arguments[] = {test_file_name, "-buffSize=12345"};
     process_return_value += process_input_arguments(2, buffer_arguments);
+
+    char *output_file_arguments[] = {test_file_name, "-out=out_file.txt"};
+    process_return_value += process_input_arguments(2, output_file_arguments);
 
     return process_return_value;
 }
