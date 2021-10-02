e05cb0b8120d4a3bd82cb25b18855e351a9261e7
Anton Kesy <anton@kesy.de>
2021-10-02 13:27:37 +0200

    add test for buffer size argument

--- test/tests/test_cli.c
+++ test/tests/test_cli.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include "../../huffman_coding_cli.h"
+#include "../test_utility.h"
 
 int test_error()
 {
@@ -8,10 +9,22 @@ int test_error()
     char *bad_arguments = "nothing :)";
     process_return_value += process_input_arguments(2, &bad_arguments);
 
+    return process_return_value == 2 ? 0 : 1;
+}
+
+int test_buffer_size()
+{
+    create_test_input_file("testfilename.txt", 0xF);
+    int process_return_value = 0;
+    char *bad_arguments[] = {"testfilename.txt", "-buffSize=12345"};
+    process_return_value += process_input_arguments(2, bad_arguments);
+
     return process_return_value;
 }
 
 int main(void)
 {
-    return 0;
+    return test_error() + test_buffer_size();
 }
+
+
